7bb98f8c99968008629c4e6d440296bd3c1970f1
daromi94 <romeromillandavidandres@gmail.com>
2022-03-29 12:16:14 -0500

    chore: scan shred packages

--- account/src/main/java/com/nebula/account/AccountApplication.java
+++ account/src/main/java/com/nebula/account/AccountApplication.java
@@ -2,10 +2,8 @@ package com.nebula.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = {"com.nebula.account", "com.nebula.shared"})
 public class AccountApplication {
 
     public static void main(String[] args) {
--- customer/src/main/java/com/nebula/customer/CustomerApplication.java
+++ customer/src/main/java/com/nebula/customer/CustomerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.nebula.customer", "com.nebula.shared"})
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.nebula.shared")
 public class CustomerApplication {
