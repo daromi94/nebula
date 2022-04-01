b62095fda5c0dd7f721ed549ee4b0fe0e996437d
daromi94 <romeromillandavidandres@gmail.com>
2022-03-31 20:47:11 -0500

    refactor: make message publisher an event bus

--- customer/src/main/java/com/nebula/customer/adapter/in/CustomerPostController.java
+++ customer/src/main/java/com/nebula/customer/adapter/in/CustomerPostController.java
@@ -1,7 +1,11 @@
-package com.nebula.customer.controller;
+package com.nebula.customer.adapter.in;
 
-import com.nebula.customer.service.CustomerCreator;
-import com.nebula.shared.customer.domain.*;
+import com.nebula.customer.application.service.CustomerCreator;
+import com.nebula.shared.customer.CustomerPostRequest;
+import com.nebula.shared.domain.Email;
+import com.nebula.shared.domain.customer.CustomerFirstName;
+import com.nebula.shared.domain.customer.CustomerId;
+import com.nebula.shared.domain.customer.CustomerLastName;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/customers")
 @Slf4j
-public final class CustomerPostController {
+final class CustomerPostController {
 
     private final CustomerCreator creator;
 
@@ -22,10 +26,12 @@ public final class CustomerPostController {
     @PostMapping
     public void post(@RequestBody CustomerPostRequest request) {
         log.info("new customer post request for {}", request);
-        CustomerId id = new CustomerId(request.id());
+
+        CustomerId        id        = new CustomerId(request.id());
         CustomerFirstName firstName = new CustomerFirstName(request.firstName());
-        CustomerLastName lastName = new CustomerLastName(request.lastName());
-        CustomerEmail email = new CustomerEmail(request.email());
+        CustomerLastName  lastName  = new CustomerLastName(request.lastName());
+        Email             email     = new Email(request.email());
+
         creator.create(id, firstName, lastName, email);
     }
 
--- shared/src/main/java/com/nebula/shared/amqp/EventBus.java
+++ shared/src/main/java/com/nebula/shared/amqp/EventBus.java
@@ -6,17 +6,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class MessagePublisher {
+public class EventBus {
 
     private final AmqpTemplate template;
 
-    public MessagePublisher(AmqpTemplate template) {
+    public EventBus(AmqpTemplate template) {
         this.template = template;
     }
 
     public void publish(Object payload, String exchange, String routingKey) {
         log.info("publishing to exchange {} using routing key {} - payload {}", exchange, routingKey, payload);
+
         template.convertAndSend(exchange, routingKey, payload);
+
         log.info("published to exchange {} using routing key {} - payload {}", exchange, routingKey, payload);
     }
 
