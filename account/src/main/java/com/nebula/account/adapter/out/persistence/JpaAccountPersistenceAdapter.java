968efee1e9956ac9f399ab190d2ff94d5e053321
daromi94 <romeromillandavidandres@gmail.com>
2022-03-31 20:52:33 -0500

    feat: implement account's jpa-based access

---
+++ account/src/main/java/com/nebula/account/adapter/out/persistence/JpaAccountPersistenceAdapter.java
@@ -0,0 +1,41 @@
+package com.nebula.account.adapter.out.persistence;
+
+import com.nebula.account.application.port.out.AccountRepository;
+import com.nebula.account.domain.Account;
+import com.nebula.shared.domain.account.AccountId;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+class JpaAccountPersistenceAdapter implements AccountRepository {
+
+    private final JpaAccountRepository repository;
+
+    private final JpaAccountMapper mapper;
+
+    public JpaAccountPersistenceAdapter(JpaAccountRepository repository, JpaAccountMapper mapper) {
+        this.repository = repository;
+        this.mapper     = mapper;
+    }
+
+    @Override
+    public void save(Account account) {
+        JpaAccount jpaAccount = mapper.fromDomain(account);
+
+        repository.saveAndFlush(jpaAccount);
+    }
+
+    @Override
+    public Optional<Account> search(AccountId id) {
+        Optional<JpaAccount> optional = repository.findById(id.value());
+
+        if (optional.isEmpty()) return Optional.empty();
+
+        JpaAccount jpaAccount = optional.get();
+        Account    account    = mapper.fromJpa(jpaAccount);
+
+        return Optional.of(account);
+    }
+
+}
