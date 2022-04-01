4915308167b44d2529385e3e63882eb52ce56767
daromi94 <romeromillandavidandres@gmail.com>
2022-03-31 20:48:58 -0500

    refactor: clean fraud-check-controller

--- fraud/src/main/java/com/nebula/fraud/adapter/in/FraudCheckController.java
+++ fraud/src/main/java/com/nebula/fraud/adapter/in/FraudCheckController.java
@@ -1,8 +1,8 @@
-package com.nebula.fraud.controller;
+package com.nebula.fraud.adapter.in;
 
-import com.nebula.fraud.service.FraudChecker;
-import com.nebula.shared.fraud.domain.FraudCheckResponse;
-import com.nebula.shared.fraud.domain.DubiousEmail;
+import com.nebula.fraud.application.service.FraudChecker;
+import com.nebula.shared.domain.Email;
+import com.nebula.shared.fraud.FraudCheckResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @Slf4j
-public final class FraudCheckController {
+final class FraudCheckController {
 
     private final FraudChecker checker;
 
@@ -23,8 +23,10 @@ public final class FraudCheckController {
     @GetMapping(path = "{email}")
     public FraudCheckResponse check(@PathVariable("email") String email) {
         log.info("new fraud check request for {}", email);
-        DubiousEmail dubiousEmail = new DubiousEmail(email);
-        boolean isFraudster = checker.check(dubiousEmail);
+
+        Email   dubiousEmail = new Email(email);
+        boolean isFraudster  = checker.check(dubiousEmail);
+
         return new FraudCheckResponse(isFraudster);
     }
 
