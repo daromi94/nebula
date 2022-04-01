ee7bac52471e88af063166c338681272cb4849e2
daromi94 <romeromillandavidandres@gmail.com>
2022-03-31 20:43:07 -0500

    refactor: move customer-post-request from domain package

---
+++ shared/src/main/java/com/nebula/shared/customer/CustomerPostRequest.java
@@ -0,0 +1,5 @@
+package com.nebula.shared.customer;
+
+public record CustomerPostRequest(String id, String firstName, String lastName, String email) {
+
+}
--- shared/src/main/java/com/nebula/shared/customer/domain/CustomerPostRequest.java
+++
@@ -1,7 +0,0 @@
-package com.nebula.shared.customer.domain;
-
-import java.util.UUID;
-
-public record CustomerPostRequest(UUID id, String firstName, String lastName, String email) {
-
-}
