4a07656a6220ef38734e0933a797b087d9f4c81f
daromi94 <romeromillandavidandres@gmail.com>
2022-03-31 20:52:03 -0500

    feat: create aggregate root abstraction

---
+++ shared/src/main/java/com/nebula/shared/domain/AggregateRoot.java
@@ -0,0 +1,27 @@
+package com.nebula.shared.domain;
+
+import com.nebula.shared.domain.bus.event.DomainEvent;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class AggregateRoot {
+
+    private List<DomainEvent> events;
+
+    public AggregateRoot() {
+        this.events = new ArrayList<>();
+    }
+
+    public final List<DomainEvent> pull() {
+        List<DomainEvent> domainEvents = this.events;
+        this.events = Collections.emptyList();
+        return domainEvents;
+    }
+
+    protected final void record(DomainEvent event) {
+        events.add(event);
+    }
+
+}
