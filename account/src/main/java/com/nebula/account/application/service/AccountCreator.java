9c4f878b92ff2c4f7f6df8450842708614f5d7de
daromi94 <romeromillandavidandres@gmail.com>
2022-03-31 20:40:15 -0500

    refactor: enforce hex-arch in account-creator use case

---
+++ account/src/main/java/com/nebula/account/application/service/AccountCreator.java
@@ -0,0 +1,34 @@
+package com.nebula.account.application.service;
+
+import com.nebula.account.application.port.out.AccountRepository;
+import com.nebula.account.domain.Account;
+import com.nebula.account.domain.AccountAlreadyExists;
+import com.nebula.shared.amqp.EventBus;
+import com.nebula.shared.domain.account.AccountId;
+import com.nebula.shared.domain.customer.CustomerId;
+import org.springframework.stereotype.Service;
+
+@Service
+public final class AccountCreator {
+
+    private final AccountRepository repository;
+
+    private final EventBus eventBus;
+
+    public AccountCreator(AccountRepository repository, EventBus eventBus) {
+        this.repository = repository;
+        this.eventBus   = eventBus;
+    }
+
+    public void create(AccountId id, CustomerId customerId) throws AccountAlreadyExists {
+        Account account = Account.create(id, customerId);
+
+        repository.search(id).ifPresent((entity) -> {
+            throw new AccountAlreadyExists(id);
+        });
+
+        repository.save(account);
+        // TODO: Publish domain events to the event bus
+    }
+
+}
