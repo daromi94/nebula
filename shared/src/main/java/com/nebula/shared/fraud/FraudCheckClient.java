b6ccf6b8fd30e1c04a5958b8f599347855fd9857
daromi94 <romeromillandavidandres@gmail.com>
2022-03-31 20:48:45 -0500

    refactor: move fraud-check-client from domain package

--- shared/src/main/java/com/nebula/shared/fraud/FraudCheckClient.java
+++ shared/src/main/java/com/nebula/shared/fraud/FraudCheckClient.java
@@ -1,6 +1,5 @@
 package com.nebula.shared.fraud;
 
-import com.nebula.shared.fraud.domain.FraudCheckResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
