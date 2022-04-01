230da0f9e879da4ea03a2f7b13d0d330c01876d4
daromi94 <romeromillandavidandres@gmail.com>
2022-03-31 20:51:54 -0500

    feat: create domain event abstraction

---
+++ shared/src/main/java/com/nebula/shared/domain/account/AccountCreated.java
@@ -0,0 +1,31 @@
+package com.nebula.shared.domain.account;
+
+import com.nebula.shared.domain.bus.event.DomainEvent;
+
+public final class AccountCreated extends DomainEvent {
+
+    private final String customerId;
+
+    private final double balance;
+
+    public AccountCreated(String id, String customerId, double balance) {
+        super(id);
+
+        this.customerId = customerId;
+        this.balance    = balance;
+    }
+
+    @Override
+    public String name() {
+        return "account.created";
+    }
+
+    public String customerId() {
+        return customerId;
+    }
+
+    public double balance() {
+        return balance;
+    }
+
+}
