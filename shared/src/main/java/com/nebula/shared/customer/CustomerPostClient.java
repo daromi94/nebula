f2496408190fbfe53fb3b18599cd1352797baf40
daromi94 <romeromillandavidandres@gmail.com>
2022-03-31 20:44:09 -0500

    refactor: move customer-post-client from domain package

--- shared/src/main/java/com/nebula/shared/customer/CustomerPostClient.java
+++ shared/src/main/java/com/nebula/shared/customer/CustomerPostClient.java
@@ -1,6 +1,5 @@
 package com.nebula.shared.customer;
 
-import com.nebula.shared.customer.domain.CustomerPostRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
