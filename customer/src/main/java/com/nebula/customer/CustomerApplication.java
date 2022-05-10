1eba16e9dd0f93671efa64990f4a85345862ca91
daromi94 <romeromillandavidandres@gmail.com>
2022-05-09 21:18:28 -0500

    refactor: remove eureka

--- customer/src/main/java/com/nebula/customer/CustomerApplication.java
+++ customer/src/main/java/com/nebula/customer/CustomerApplication.java
@@ -3,12 +3,8 @@ package com.nebula.customer;
 import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.nebula.customer", "com.nebula.shared"})
-@EnableEurekaClient
-@EnableFeignClients(basePackages = "com.nebula.shared")
 class CustomerApplication {
 
   public static void main(String[] args) {
--- fraud/src/main/java/com/nebula/fraud/FraudApplication.java
+++ fraud/src/main/java/com/nebula/fraud/FraudApplication.java
@@ -3,10 +3,8 @@ package com.nebula.fraud;
 import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = {"com.nebula.fraud", "com.nebula.shared"})
-@EnableEurekaClient
 class FraudApplication {
 
   public static void main(String[] args) {
